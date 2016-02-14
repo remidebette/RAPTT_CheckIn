d5bac8d870f1589aec6f2d0d729f38d1cc3429cb
remidebette <remi.debette@gmail.com>
2016-02-14 20:07:20 +0900

    ajout d'un broadcast pour l'alarme Ca ne marche pas terrible

--- app/src/main/java/com/raptt/debette/raptt_checkin/MainActivity.java
+++ app/src/main/java/com/raptt/debette/raptt_checkin/MainActivity.java
@@ -1,113 +1,63 @@
 package com.raptt.debette.raptt_checkin;
 
 import android.app.AlarmManager;
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.ToggleButton;
 
 import java.util.Calendar;
 
 
 public class MainActivity extends MenuActivity {
-    public int ID_NOTIFICATION = 0;
-    public final static String ICON = "com.raptt.debette.raptt_checkin.extra.icon";
 
-    // La chaîne de caractères par défaut
-    private final String defaut = "Vous devez cliquer sur le bouton « Calculer l'IMC » pour obtenir un résultat.";
+    AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
+    private TimePicker alarmTimePicker;
+    private static MainActivity inst;
+    private TextView alarmTextView;
 
-
-    Button envoyer = null;
-    Button raz = null;
-
-    EditText poids = null;
-    EditText taille = null;
-
-    RadioGroup group = null;
-
-    TextView result = null;
-
-
-    private TextWatcher textWatcher = new TextWatcher() {
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-            result.setText(defaut);
-        }
-
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-        }
-    };
-
-    private OnClickListener envoyerListener = new OnClickListener() {
-
-        @Override
-        public void onClick(View v) {
-            result.setText("Votre IMC est ");
-        }
-    };
+    public static MainActivity instance() {
+        return inst;
+    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        alarmTimePicker = (TimePicker) findViewById(R.id.alarmTimePicker);
+        alarmTextView = (TextView) findViewById(R.id.alarmText);
+        ToggleButton alarmToggle = (ToggleButton) findViewById(R.id.alarmToggle);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+    }
 
-        // On récupère toutes les vues dont on a besoin
-        envoyer = (Button) findViewById(R.id.calcul);
+    public void onToggleClicked(View view) {
+        if (((ToggleButton) view).isChecked()) {
+            Log.d("MyActivity", "Alarm On");
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+            calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+            Intent myIntent = new Intent(MainActivity.this, AlarmReceiver.class);
+            pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, myIntent, 0);
+            alarmManager.set(AlarmManager.RTC, calendar.getTimeInMillis(), pendingIntent);
+        } else {
+            alarmManager.cancel(pendingIntent);
+            setAlarmText("");
+            Log.d("MyActivity", "Alarm Off");
+        }
+    }
 
-        taille = (EditText) findViewById(R.id.taille);
-        poids = (EditText) findViewById(R.id.poids);
-
-        group = (RadioGroup) findViewById(R.id.group);
-
-        result = (TextView) findViewById(R.id.result);
-
-        // On attribue un listener adapté aux vues qui en ont besoin
-        envoyer.setOnClickListener(envoyerListener);
-        taille.addTextChangedListener(textWatcher);
-        poids.addTextChangedListener(textWatcher);
-
-        raz = (Button) findViewById(R.id.raz);
-        raz.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // L'icône sera une petite loupe
-                int icon = R.drawable.ic_line_a;
-                // Le premier titre affiché
-                CharSequence tickerText = "Prise de service";
-                // Daté de maintenant
-                long when = System.currentTimeMillis();
-
-                AlarmManager alarmManager = (AlarmManager) MainActivity.this.getSystemService(MainActivity.this.ALARM_SERVICE);
-                Calendar calendar =  Calendar.getInstance();
-                calendar.set(Calendar.SECOND, 5);
-                //calendar.set(int year, int month, int date, int hour, int minute, int second);
-
-                // La notification est crée
-                Intent intent = new Intent(MainActivity.this, ReminderService.class);
-                //intent.setAction(ReminderService.ACTION_SET_NOTIFICATION);
-                PendingIntent pendingIntent = PendingIntent.getService(MainActivity.this, 0, intent, 0);
-                alarmManager.set(AlarmManager.ELAPSED_REALTIME, calendar.getTimeInMillis(), pendingIntent);
-            }
-        });
-
+    public void setAlarmText(String alarmText) {
+        alarmTextView.setText(alarmText);
     }
 }
--- app/src/main/java/com/raptt/debette/raptt_checkin/ReminderService.java
+++ app/src/main/java/com/raptt/debette/raptt_checkin/ReminderService.java
@@ -9,49 +9,45 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.preference.PreferenceManager;
-import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
  * a service on a separate handler thread.
  * <p/>
- * TODO: Customize class - update intent actions, extra parameters and static
- * helper methods.
+ * TODO: Customize class - update intent actions and extra parameters.
  */
 public class ReminderService extends IntentService {
 
-    private static final int NOTIF_ID = 1;
-
-    // TODO: Rename actions, choose action names that describe tasks that this
-    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-        public static final String ACTION_SET_NOTIFICATION = "com.raptt.debette.raptt_checkin.action.FOO";
-
-    // TODO: Rename parameters
-    //private static final String EXTRA_PARAM1 = "com.raptt.debette.raptt_checkin.extra.PARAM1";
-    //private static final String EXTRA_PARAM2 = "com.raptt.debette.raptt_checkin.extra.PARAM2";
+    private NotificationManager nm;
 
     public ReminderService() {
         super("ReminderService");
     }
 
-
-
-
     @Override
-    protected void onHandleIntent(Intent intent) {
-        if (intent != null) {
-                final int icon = intent.getIntExtra(MainActivity.ICON, -1);
-                handleActionFoo(icon);
-            }
-        }
+    public void onHandleIntent(Intent intent) {
+        sendNotification("Wake Up! Wake Up!");
+    }
+
+    private void sendNotification(String msg) {
+        Log.d("AlarmService", "Preparing to send notification...: " + msg);
+        nm = (NotificationManager) this
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainActivity.class), 0);
+
+        /*NotificationCompat.Builder alamNotificationBuilder = new NotificationCompat.Builder(
+                this).setContentTitle("Alarm").setSmallIcon(R.drawable.ic_launcher)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+                .setContentText(msg);*/
 
 
-    /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionFoo(int icon) {
+        //alarmNotificationManager.notify(1, alamNotificationBuilder.build());
+
+
         // TODO: Handle action Foo
         /*NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         long when = System.currentTimeMillis();         // notification time
@@ -63,21 +59,18 @@ public class ReminderService extends IntentService {
         //notification.setLatestEventInfo(getApplicationContext(), "It's about time", "You should open the app now", contentIntent);
         nm.notify(NOTIF_ID, notification);*/
 
-        final Intent notificationIntent = new Intent(ReminderService.this, ReminderService.class);
-        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
 
-        final PendingIntent contentIntent = PendingIntent.getActivity(ReminderService.this, 0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+        long when = System.currentTimeMillis();
 
-
-        NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         Resources res = ReminderService.this.getResources();
 
         Notification.Builder nb = new Notification.Builder(ReminderService.this);
         nb.setContentTitle("title");
         nb.setContentText("message");
+        //nb.setWhen(when);
+        nb.setContentIntent(contentIntent);
         nb.setSmallIcon(R.drawable.ic_line_a);
-        nb.setWhen(System.currentTimeMillis());
         nb.setAutoCancel(true);
         nb.setTicker("message");
 
@@ -100,8 +93,8 @@ public class ReminderService extends IntentService {
 
         Notification notification = nb.build();
 
-        nm.notify(NOTIF_ID, notification);
+        nm.notify(1, notification);
+
+        Log.d("AlarmService", "Notification sent.");
     }
-
-
 }
