f95937f420675b19d9954f4b174aef0a3ab6869e
remidebette <remi.debette@gmail.com>
2016-02-15 21:49:32 +0900

    La notification fonctionne maintenant parfaitement

--- app/src/main/java/com/raptt/debette/raptt_checkin/ReminderService.java
+++ app/src/main/java/com/raptt/debette/raptt_checkin/ReminderService.java
@@ -6,9 +6,10 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
-import android.media.RingtoneManager;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 
@@ -39,65 +40,32 @@ public class ReminderService extends IntentService {
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
                 new Intent(this, MainActivity.class), 0);
 
-        /*NotificationCompat.Builder alamNotificationBuilder = new NotificationCompat.Builder(
-                this).setContentTitle("Alarm").setSmallIcon(R.drawable.ic_launcher)
-                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
-                .setContentText(msg);*/
-
-
-        //alarmNotificationManager.notify(1, alamNotificationBuilder.build());
-
-
-        // TODO: Handle action Foo
-        /*NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        long when = System.currentTimeMillis();         // notification time
-        Notification notification = new Notification(icon, "reminder", when);    //  Extra should be R.drawable.ic_line_a
-        notification.defaults |= Notification.DEFAULT_SOUND;
-        notification.flags |= notification.FLAG_AUTO_CANCEL;
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-        //notification.setLatestEventInfo(getApplicationContext(), "It's about time", "You should open the app now", contentIntent);
-        nm.notify(NOTIF_ID, notification);*/
-
-
-        long when = System.currentTimeMillis();
-
-
         Resources res = ReminderService.this.getResources();
 
         Notification.Builder nb = new Notification.Builder(ReminderService.this);
         nb.setContentTitle("title");
         nb.setContentText("message");
-        //nb.setWhen(when);
         nb.setContentIntent(contentIntent);
         nb.setSmallIcon(R.drawable.ic_line_a);
         nb.setAutoCancel(true);
         nb.setTicker("message");
 
-        long[] v = {500, 1000};
-        nb.setVibrate(v);
+        // Check Ringtone
+        SharedPreferences preference = PreferenceManager.getDefaultSharedPreferences(ReminderService.this);
+        String strRingtonePreference = preference.getString("notifications_new_message_ringtone", "DEFAULT_SOUND");
+        nb.setSound(Uri.parse(strRingtonePreference));
 
-        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        nb.setSound(uri);
+        int notificationDefault = Notification.DEFAULT_LIGHTS;
 
-        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-        if (alarmUri == null) {
-            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        // Check vibrate
+        boolean isVibrate = preference.getBoolean("notifications_new_message_vibrate", true);
+        if (isVibrate) {
+            notificationDefault = notificationDefault | Notification.DEFAULT_VIBRATE;
         }
 
-        //final Uri ringtone = Uri.parse(PreferenceManager.getDefaultSharedPreferences(ReminderService.this).getString("ringtone", getString(R.string.settings_default_ringtone)));
-
-        nb.setDefaults(Notification.DEFAULT_VIBRATE);
-        nb.setSound(uri);
-        nb.setDefaults(Notification.DEFAULT_LIGHTS);
-
-
-
-        //nb.setContentIntent(contentIntent);
-
+        nb.setDefaults(notificationDefault);
 
         Notification notification = nb.build();
-
         nm.notify(1, notification);
 
         Log.d("AlarmService", "Notification sent.");
